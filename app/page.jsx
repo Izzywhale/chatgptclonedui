37a733ac3ae7726b62e7a27ead2c95cbe89c2c93
Izzywhale <osobajoisrael@gmail.com>
2024-04-25 21:11:37 +0100

    initial changes

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+import MainPage from "@/components/MainPage";
+import MobileMenu from "@/components/MobileMenu";
+
+export default function Home() {
+  return (
+    <aside className="w-full h-full lg:h-screen lg:overflow-hidden">
+      <MobileMenu />
+      <MainPage />
+    </aside>
+  );
+}
